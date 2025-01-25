f6b599014fb4bee0e6cc1d3793423894cb754f5f
khubaibkm <khubaibahmadkm@gmail.com>
2025-01-25 17:41:55 +0530

    opengraph details updated

--- app/layout.js
+++ app/layout.js
@@ -5,16 +5,16 @@ export const metadata = {
   description: "Showcasing the projects, skills, and expertise of Khubaib Ahmad, a web developer specializing in Next.js, Node.js, and modern web technologies.",
   openGraph: {
     type: "website",
-    title: "Khubaib Ahmad | Web Developer Portfolio",
-    description: "Explore the professional portfolio of Khubaib Ahmad, featuring innovative projects and technical expertise.",
+    title: "Khubaib Ahmad | Web Developer",
+    description: "Explore the Khubaib Ahmad's professional journey through his Portfolio.",
     url: "https://khubaib-live.vercel.app",
-    images: ["https://media.licdn.com/dms/image/v2/D5616AQFIrUaxv1xD6g/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1737291999380?e=1743033600&v=beta&t=2Vy02G6XuADNz3FdPkZfAEcmbR6SW0uk_sn-kDpaxZA"],
+    images: ["https://media.licdn.com/dms/image/v2/D5603AQH8hjCLTYIfdA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1722962360183?e=1743033600&v=beta&t=7wE7xuzZbunBYTJ0yuUcX-iIYCprp2zj9m58sGynLkY"],
   },
   twitter: {
     card: "summary_large_image",
     title: "Khubaib Ahmad | Web Developer Portfolio",
     description: "Learn more about Khubaib Ahmad and his professional journey.",
-    images: ["https://media.licdn.com/dms/image/v2/D5616AQFIrUaxv1xD6g/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1737291999380?e=1743033600&v=beta&t=2Vy02G6XuADNz3FdPkZfAEcmbR6SW0uk_sn-kDpaxZA"],
+    images: ["https://media.licdn.com/dms/image/v2/D5603AQH8hjCLTYIfdA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1722962360183?e=1743033600&v=beta&t=7wE7xuzZbunBYTJ0yuUcX-iIYCprp2zj9m58sGynLkY"],
   },
   robots: {
     index: true,
